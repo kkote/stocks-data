52fbd6c54aaf03df42da5f94041718250d928ac3
kdkote <kaygray521@gmail.com>
2019-09-02 21:42:22 -0500

    Add API Response to Database

--- download.py
+++ download.py
@@ -1,58 +1,49 @@
 
-from datetime import datetime
-import config as c
 import json
 import requests
+import json
+import config as c
+from datetime import date, datetime, timedelta
 from requests.exceptions import HTTPError
 
-def constructEODurl(ticker,start_date,end_date,freq):
-
-    start=datetime.strptime(start_date, "%Y-%m-%d")
-    end = datetime.strptime(end_date, "%Y-%m-%d")
-    start = str(start_date)
-    end = str(end_date)    
-    apikey = c.get_keys()
-
-    EODurl= f"https://eodhistoricaldata.com/api/eod/{ticker}.US?from={start}&to={end}&api_token={apikey}&period={freq}&fmt=json"
-    #  EODurl= f"https://eodhistoricaldata.com/api/eod/{ticker}.US?to={end}&api_token={apikey}&period=m&fmt=json"
-   
-    return EODurl
 
 
-def download(filePath,urlOfFile):
-    # import requests
-    # from requests.exceptions import HTTPError
+def get_api_data(URL, PARAMS):
 
-    print(urlOfFile)
-    
-    # response = requests.get(urlOfFile)
-    # json_response = response.json()
-    json_response=[{'date': '2019-08-05', 'open': 2, 'high': 21, 'low': 20, 'close': 208, 'adjusted_close': 207, 'volume': 54}, {'date': '2019-08-06', 'open': 205.53, 'high': 206.43, 'low': 201.63, 'close': 204.02, 'adjusted_close': 203.2478, 'volume': 38688200}]
+    r = requests.get(url = URL, params = PARAMS) 
+    data = r.json() 
 
-    print(json_response)
-    return json_response
-
-    # print(json_response)
+    return data
 
 
-def insertIntoDB(api_data):
+def insert_data_db(api_data):
     import mysql.connector
-    from datetime import date, datetime, timedelta
+    from mysql.connector import errorcode, Error
 
-    mydb = mysql.connector.connect(
-        database='test'
-    )
-
-    cur = mydb.cursor()
-
-    insert_ticker = ("INSERT INTO testTable "
+    pwd = c.db_password()
+    db = '******'
+    user='****'
+    host='*****'
+    
+    insert_row = ("INSERT INTO tableS "
                     "(ticker,date, open,high,low, close, adjusted_close,volume) "
                     "VALUES (%(ticker)s, %(date)s, %(open)s, %(high)s, %(low)s, %(close)s, %(adjusted_close)s, %(volume)s)")
-    # the_date = datetime(2019, 8, 1)
-    cur.execute(insert_ticker, api_data)
 
-    emp_no = cur.lastrowid
+    try:
+        conn = mysql.connector.connect(
+            database = db,
+            user=user,
+            host=host,
+            password=pwd
+        )
 
-    mydb.commit()
-    cur.close()
-    mydb.close()
+        cur = conn.cursor()
+        cur.executemany(insert_row,api_data)
+        conn.commit()
+
+    except Error as error:
+      print(error)
+    
+    finally:
+      cur.close()
+      conn.close()
--- run.py
+++ run.py
@@ -1,34 +1,47 @@
-from download import constructEODurl
-from download import download
-from download import insertIntoDB
+from download import get_api_data, insert_data_db
+from datetime import datetime, date
+import config as c
 import json
+from time import perf_counter
 
+
+# start_time = time.clock()
+t1_start = perf_counter()  
 
 # file='tickers.txt'
-file='forTestTickers.txt'
+file = 'forTestTickers.txt'
+# tickers = ['AAPL']
 
-def tickerLength(file):
+def get_tickers(file):
     with open(file, 'r') as filehandle:
         basicList = json.load(filehandle)
         print(len(basicList))
         return basicList
 
-tickers=tickerLength(file)
+tickers = get_tickers(file)
 
 
-start_date = "2019-08-01"
-end_date = "2019-08-03"
-freq = "d"
-# frequency d = daily, w= weekly, m=monthly
+start = "2019-08-30"
+end = str(date.today())
+apikey = c.get_keys()
+freq='d'
 
+eod_params = { 'from':start,'to':end,'api_token':apikey,'period':freq,'fmt':'json'} 
 
-for ticker in tickers:
-    # print(ticker)
-    localFilePath="CSV/theTestLoop.txt"
-    EodUrl = constructEODurl(ticker,start_date,end_date,freq)
+def add_to_db(tickers,params):
 
-    json_response = download(localFilePath,EodUrl)
+        for ticker in tickers:
+                eod_url= f"https://eodhistoricaldata.com/api/eod/{ticker}.US"
+                
+                json_response = get_api_data(eod_url, params)
 
-    for i in json_response:
-        i.update({'ticker':ticker})
-        insertIntoDB(i)
+                for i in json_response:
+                         i.update({'ticker':ticker})
+
+                print(json_response)
+                insert_data_db(i)
+
+add_to_db(tickers,eod_params)
+
+t1_stop = perf_counter() 
+print(t1_start-t1_stop, "seconds")
